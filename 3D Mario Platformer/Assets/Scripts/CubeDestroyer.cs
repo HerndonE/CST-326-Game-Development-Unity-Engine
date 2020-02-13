b76f4ca20df189b93d116106e7d05eb70b4ab72a
HerndonE <EHerndon17@gmail.com>
2020-02-12 18:01:29 -0800

    Mario Lab upload

---
+++ 3D Mario Platformer/Assets/Scripts/CubeDestroyer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CubeDestroyer : MonoBehaviour
+{
+    public Text scoreText;
+    public Text coinText;
+    public int points = 0;
+    public int coinPoints = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                BoxCollider boxCollider = hit.collider as BoxCollider;
+                if(boxCollider.tag != null)
+                {
+                    Destroy(boxCollider.gameObject);
+                    points = points + 10;
+                    //scoreText.text = "" + points;
+                    scoreText.text = "0000" + points;
+                    if (points >= 10)
+                    {
+                        scoreText.text = "000" + points;
+                    }
+                    if (points >= 99)
+                    {
+                       
+                     scoreText.text = "00" + points;
+                    }
+                }
+                if (boxCollider.tag == "qbox")
+                {
+                    Destroy(boxCollider.gameObject);
+                    //Debug.Log("We heed your call");
+                    coinPoints = coinPoints + 1;
+                    coinText.text = "Coins x " + coinPoints;
+                }
+                   
+            }
+        }
+    }
+}
---
+++ 3D Mario Platformer/Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    float timeLeft = 375.0f;
+    public Text text;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+}
