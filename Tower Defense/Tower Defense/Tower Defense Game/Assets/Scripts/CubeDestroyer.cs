67ae643b4140fa4ded3afb7f27bf29e31e893a40
HerndonE <EHerndon17@gmail.com>
2020-03-20 01:07:11 -0700

    Tower Defense pt 1 Upload

---
+++ Tower Defense/Tower Defense/Tower Defense Game/Assets/Scripts/CubeDestroyer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CubeDestroyer : MonoBehaviour
+{
+    public Text scoreText;
+    public Text coinText;
+    public int points = 0;
+    public int cointPoints = 0;
+    public int enemyAHealth;
+    public int enemyBHealth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                BoxCollider boxCollider = hit.collider as BoxCollider;
+                if(boxCollider.tag != null)
+                {
+                    enemyAHealth--;
+                    Debug.Log(enemyAHealth);
+                    if (enemyAHealth < 0)
+                    {
+                        Destroy(boxCollider.gameObject);
+                        points = points + 10;
+                        scoreText.text = "Score:" + points;
+
+                        cointPoints = cointPoints + 1;
+                        coinText.text = "Coins:" + cointPoints;
+                    }
+                }
+                if (hit.collider.tag == "enemyB")
+                {
+                    enemyBHealth--;
+                    Debug.Log(enemyBHealth);
+                    if (enemyBHealth == 0)
+                    {
+                        Destroy(boxCollider.gameObject);
+                        points = points + 10;
+                        scoreText.text = "Score:" + points;
+
+                        cointPoints = cointPoints + 2;
+                        coinText.text = "Coins:" + cointPoints;
+
+                    }
+                }
+                   
+            }
+        }
+    }
+}
---
+++ Tower Defense/Tower Defense/Tower Defense Game/Assets/Scripts/HealthCare.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthCare : MonoBehaviour
+{
+    public int health = 0;
+    public Text scoreText;
+    public Text coinText;
+    public int points = 0;
+    public int cointPoints = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GameObject.Find("Score").GetComponent<Text>();
+        coinText = GameObject.Find("Coin").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        takeScore();
+    }
+
+    void takeScore()
+    {
+        if (health == 0)
+        {
+            health++;
+            Destroy(gameObject);
+           
+           
+
+        }
+    }
+
+}
